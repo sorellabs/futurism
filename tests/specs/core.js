b6c5c928a3f2ca97e4ac2ad2698fb41451dceae1
Quildreen Motta <quildreen@gmail.com>
2013-09-20 13:19:29 -0300

    Adds tests for bringing in Promises/A+ thenables into the monadic world.

--- tests/specs/core.js
+++ tests/specs/core.js
@@ -1,5 +1,6 @@
 var spec = require('brofist')()
 var expect = require('chai').expect
+var pinky = require('pinky')
 var P = require('../../')
 
 module.exports = spec('Promise(a)', function(it, spec) {
@@ -38,6 +39,23 @@ module.exports = spec('Promise(a)', function(it, spec) {
 
     it('right identity', function() {
       expect(p1.chain(P.of).chain(id)).to.equal(p1.chain(id))
+    })
+  })
+
+  spec('from(a)', function(it) {
+    it('Given a Promise, shouldn\'t wrap.', function() {
+      expect(P.from(p1)).to.equal(p1)
+    })
+
+    it('Given a Thenable (Promises/A+), should grab the flat value.', function() {
+      var t1 = pinky(pinky(1))
+      var done = pinky()
+      P.from(t1).chain(function(a) {
+        console.log(a)
+        if (a === 1)  done.fulfill()
+        else          done.reject('a ≠ 1')
+      })
+      return done
     })
   })
 
